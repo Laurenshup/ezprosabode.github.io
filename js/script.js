f29ac21436bd507ace3441af2ace7acf22e756c3
BuildTools <unconfigured@null.spigotmc.org>
2017-04-03 21:37:15 +0200

    Second Test Upload

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function bgScroll() {
-    "use strict";
-    var bg = document.getElementById("header");
-    bg.style.backgroundPositionY = -(window.pageYOffset / 4) + "px";
-}
-window.addEventListener("scroll", bgScroll, false);
